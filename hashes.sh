a08383b001fd3f9ec42498728229c0dc90bb93ea
Aseem Rastogi <aseemr@microsoft.com>
2019-02-11 17:04:20 +0530

    update hashes

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=4b6b5c3e5c70e0f905a32c65fa231ae6fdc32c4f
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=0ded67c33cdd8824aedefb44c964dce4ba86d839
+branches[mitls-fstar]=_aseem_cache_smt
 hashes[quackyducky]=a9e4f96faa9c24cb8c6d146f8a880fe3522c956a
 branches[quackyducky]=master
-hashes[FStar]=d37e991bb6d0b6e9b3f66b3ec7ee520713a0223e
-branches[FStar]=master
+hashes[FStar]=dd8618f00c7cd876db571126ed8edb53528c8894
+branches[FStar]=_aseem_cache_smt
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=b9f9a0854f746e5a5412f1a5d1e5b10dfd64376e
-branches[hacl-star]=fstar-master
+hashes[hacl-star]=79e129d91bf55c8faca89c45c426b7455a46bb86
+branches[hacl-star]=_aseem_cache_smt
 hashes[kremlin]=d8ba3898f9b0269b671a72d6e8ed0dff75371965
 branches[kremlin]=master
